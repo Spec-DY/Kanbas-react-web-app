a7178c4756e87678225964af845859d85d7edd14
Spec-DY <ibonehp@gmail.com>
2024-07-09 11:44:27 +0800

    Complete A1

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -3,7 +3,13 @@ export default function CourseStatus() {
     <div id="wd-course-status">
     <h2>Course Status</h2>
     <button>Unpublish</button> <button>Publish</button>
-    {/* Complete on your own */}
-    <button>View Course Notifications</button>
+    
+    <button style={{ display: 'block', marginTop: '10px' }}>Import Existing Content</button>
+    <button style={{ display: 'block'}}>Import from Commons</button>
+    <button style={{ display: 'block'}}>Choose Home Page</button>
+    <button style={{ display: 'block'}}>View Course Stream</button>
+    <button style={{ display: 'block'}}>New Announcement</button>
+    <button style={{ display: 'block'}}>New Analytics</button>
+    <button style={{ display: 'block'}}>View Course Notifications</button>
     </div>
 );}
