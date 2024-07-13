6c6ba93e4b541a15a27161dae1f508e86848ea8b
Spec-DY <ibonehp@gmail.com>
2024-07-13 23:37:23 +0800

    Develop Course navigation bar

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -2,12 +2,13 @@ import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 import { Routes, Route, Navigate } from "react-router";
 import Courses from "./Courses";
+import "./styles.css";
 
 export default function Kanbas() {
     return (
         <div id="wd-kanbas">
             <KanbasNavigation />
-            <div>
+            <div className="wd-main-content-offset p-3">
                 <Routes>
                     <Route path="/" element={<Navigate to="Dashboard" />} />
                     <Route path="Account" element={<h1>Account</h1>} />
