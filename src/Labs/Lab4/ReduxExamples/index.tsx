a8c5ba6e9881150088365e55b7e64241fa036ea4
Spec-DY <ibonehp@gmail.com>
2024-07-29 13:01:30 -0700

    add: redux counter

--- src/Labs/Lab4/ReduxExamples/index.tsx
+++ src/Labs/Lab4/ReduxExamples/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import HelloRedux from "./HelloRedux";
+import CounterRedux from "./CounterRedux";
 
 
 export default function ReduxExamples() {
@@ -7,6 +8,7 @@ export default function ReduxExamples() {
     <div>
       <h2>Redux Examples</h2>
       <HelloRedux/>
+      <CounterRedux/>
     </div>
   );
 };
