7ee26718f85838814475b4cfa6e90c22fb5aba1f
Spec-DY <ibonehp@gmail.com>
2024-07-29 12:30:30 -0700

    a4 created

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -4,20 +4,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+import Lab4 from "./Lab4";
+import store from "./store";
+import { Provider } from "react-redux";
 
 export default function Labs() {
     console.log('Hello World!');
 return (
-    <div id="wd-labs">
-        <h1>Dingyang Jin</h1>
-        <h2>Section 01</h2>
-        <h1>Labs</h1>
-        <TOC />
-        <Routes>
-            <Route path="/" element={<Navigate to="Lab1" />} />
-            <Route path="Lab1" element={<Lab1 />} />
-            <Route path="Lab2" element={<Lab2 />} />
-            <Route path="Lab3/*" element={<Lab3 />} />
-        </Routes>
-    </div>
+    <Provider store={store}>
+        <div id="wd-labs">
+            <h1>Dingyang Jin</h1>
+            <h2>Section 01</h2>
+            <h1>Labs</h1>
+            <TOC />
+            <Routes>
+                <Route path="/" element={<Navigate to="Lab1" />} />
+                <Route path="Lab1" element={<Lab1 />} />
+                <Route path="Lab2" element={<Lab2 />} />
+                <Route path="Lab3/*" element={<Lab3 />} />
+                <Route path="Lab4" element={<Lab4 />} />
+            </Routes>
+        </div>
+    </Provider>
 );} 
