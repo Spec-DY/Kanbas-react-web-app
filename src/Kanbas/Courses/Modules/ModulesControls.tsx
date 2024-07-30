4a5f49f32fe735b9efa91676098a75e4923cc21d
Spec-DY <ibonehp@gmail.com>
2024-07-29 17:56:12 -0700

    add:create and delete module

--- src/Kanbas/Courses/Modules/ModulesControls.tsx
+++ src/Kanbas/Courses/Modules/ModulesControls.tsx
@@ -1,13 +1,27 @@
 import { FaPlus } from "react-icons/fa";
 import GreenCheckmark from "./GreenCheckmark";
+import ModuleEditor from "./ModuleEditor";
 
-export default function ModulesControls() {
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default function ModulesControls({ moduleName, setModuleName, addModule }:
+    { moduleName: string; setModuleName: (title: string) => void; addModule: () => void; }) {
+
     return (
         <div id="wd-modules-controls" className="text-nowrap">
-            <button id="wd-add-module-btn" className="btn btn-lg btn-danger me-1 float-end">
-                <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
-                Module
+            <button 
+            id="wd-add-module-btn" 
+            className="btn btn-lg btn-danger me-1 float-end"
+            data-bs-toggle="modal" 
+            data-bs-target="#wd-add-module-dialog">
+            
+            <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
+            Module
+                
             </button>
+            
             <div className="dropdown d-inline me-1 float-end">
                 <button
                     id="wd-publish-all-btn"
@@ -18,6 +32,7 @@ export default function ModulesControls() {
                     <GreenCheckmark />
                     Publish All
                 </button>
+                
                 <ul className="dropdown-menu">
                     <li>
                         <a id="wd-publish-all-modules-and-items-btn" className="dropdown-item" href="#">
@@ -48,7 +63,11 @@ export default function ModulesControls() {
             </button>
             <button id="wd-collapse-all" className="btn btn-lg btn-primary me-1 float-end">
                 Collapse All
+                
             </button>
+            <ModuleEditor dialogTitle="Add Module" moduleName={moduleName}
+                    setModuleName={setModuleName} addModule={addModule} />
         </div>
+        
     );
 }
--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -5,23 +5,35 @@ import { BsGripVertical } from "react-icons/bs";
 import { useParams } from "react-router";
 import * as db from "../../Database";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
 
 export default function Modules() {
     const { cid } = useParams();
-    const modules = db.modules;
+    const [modules, setModules] = useState<any[]>(db.modules);
+    const [moduleName, setModuleName] = useState("");
+    const addModule = () => {
+      setModules([ ...modules, { _id: new Date().getTime().toString(),
+                                       name: moduleName, course: cid, lessons: [] } ]);
+      setModuleName("");
+    };
+
+    const deleteModule = (moduleId: string) => {
+        setModules(modules.filter((m) => m._id !== moduleId));
+    };
 
     return (
         <div id="wd-modules">
-            <ModulesControls /><br /><br /><br /><br />
+            <ModulesControls setModuleName={setModuleName} moduleName={moduleName} addModule={addModule}/><br /><br /><br /><br />
             <ul className="list-group rounded-0">
                 {modules
-                    .filter((module: any) => module.course === cid)
+                    .filter((module) => module.course === cid)
                     .map((module: any) => (
                         <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray" key={module.id}>
                             <div className="wd-title p-3 ps-2 bg-secondary">
                                 <BsGripVertical className="me-2 fs-3" />
                                 {module.name}
-                                <ModuleControlButtons />
+                                <ModuleControlButtons moduleId={module._id}
+                                    deleteModule={deleteModule}/>
                             </div>
                             {module.lessons && (
                                 <ul className="wd-lessons list-group rounded-0">
