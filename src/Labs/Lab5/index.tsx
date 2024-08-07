27c0b0cf00bc2779e2919658494017a3b5fe1ca8
Spec-DY <ibonehp@gmail.com>
2024-08-06 18:51:54 -0700

    fix

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,5 +1,9 @@
 import EnvironmentVariables from "./EnvironmentVariables";
+import HttpClient from "./HttpClient";
 import PathParameters from "./PathParameters";
+import QueryParameters from "./QueryParameters";
+import WorkingWithArrays from "./WorkingWithArrays";
+import WorkingWithObjects from "./WorkingWithObjects";
 
 
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
@@ -16,6 +20,10 @@ export default function Lab5() {
         </div><hr/>
         <EnvironmentVariables />
         <PathParameters />
+        <QueryParameters />
+        <WorkingWithObjects />
+        <WorkingWithArrays />
+        <HttpClient />
       </div>
     );
   }
