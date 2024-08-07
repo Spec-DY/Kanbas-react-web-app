d9cfbf07999ba828c50162c77a592946c83eca38
Spec-DY <ibonehp@gmail.com>
2024-08-06 20:57:26 -0700

    add: error handling

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -3,7 +3,9 @@ import HttpClient from "./HttpClient";
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
 import WorkingWithArrays from "./WorkingWithArrays";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
 import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
 
 
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
@@ -23,7 +25,9 @@ export default function Lab5() {
         <QueryParameters />
         <WorkingWithObjects />
         <WorkingWithArrays />
-        {/* <HttpClient /> */}
+        <HttpClient />
+        <WorkingWithObjectsAsynchronously />
+        <WorkingWithArraysAsynchronously />
       </div>
     );
   }
